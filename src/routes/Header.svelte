3eb5f8226a68c0b9c5930a507cae934ecbade020
Till-JS <101404291+Till-JS@users.noreply.github.com>
2023-12-02 18:33:28 +0100

    new nav

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,129 +1,23 @@
-<script>
-	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
-</script>
-
-<header>
-	<div class="corner">
-		<!-- <a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a> -->
-	</div>
-
-	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
-		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Chart.js</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/tailwind' ? 'page' : undefined}>
-				<a href="/tailwind">Tailwind</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
-			</li>
-		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
-	</nav>
-
-	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
-			<img src={github} alt="GitHub" />
+<nav class="bg-gray-800 p-3 rounded-lg shadow-md mx-auto my-4 max-w-6xl">
+	<div class="flex items-center justify-between">
+	  <!-- Logo placeholder on the left -->
+	  <a href="/" class="text-white font-bold py-2 px-4 rounded hover:bg-gray-700 transition-colors duration-150 no-underline" role="button">
+		LOGO
+	  </a>
+  
+	  <!-- Buttons on the right -->
+	  <div class="flex space-x-2">
+		<a href="/link1" class="text-white font-semibold py-2 px-4 rounded hover:bg-gray-700 transition-colors duration-150 no-underline" role="button">
+		  Button 1
 		</a>
+		<a href="/link2" class="text-white font-semibold py-2 px-4 rounded hover:bg-gray-700 transition-colors duration-150 no-underline" role="button">
+		  Button 2
+		</a>
+		<a href="/link3" class="text-white font-semibold py-2 px-4 rounded hover:bg-gray-700 transition-colors duration-150 no-underline" role="button">
+		  Button 3
+		</a>
+		<!-- Add more buttons as needed -->
+	  </div>
 	</div>
-</header>
-
-<style>
-	header {
-		display: flex;
-		justify-content: space-between;
-	}
-
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
-
-	nav {
-		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
-	}
-
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--background);
-		background-size: contain;
-	}
-
-	li {
-		position: relative;
-		height: 100%;
-	}
-
-	li[aria-current='page']::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
-	}
-
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 0.5rem;
-		color: var(--color-text);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
-
-	a:hover {
-		color: var(--color-theme-1);
-	}
-</style>
+  </nav>
+  
