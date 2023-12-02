cbd92a95ba205514160b7578c0aa08649d87b925
Simon Stratemeier <simon@concept-hero.de>
2023-12-02 19:56:07 +0100

    Add article body

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,28 +12,38 @@
 </svelte:head>
 
 <section>
-	<Chart />
+	<div class="flex space-x-16">
+		<div class="infocard p-4 w-80 text-white">
+			<div class="mb-4 flex justify-between">
+				<div class="">Thema:</div>
+				<div class="p-2 rounded-md bg-neutral-800">Asyl Politik</div>
+			</div>
+			<div class="mb-4 flex justify-between">
+				<span class="">Partei:</span>
+				<span class="p-2 rounded-md bg-blue-700">JUSO</span>
+			</div>
+			<div class="mb-4 flex justify-between">
+				<span class="">Politisches Spektrum:</span>
+				<span class="p-2 rounded-md bg-green-700">65% Links</span>
+			</div>
+			<div class="mb-4 flex justify-between">
+				<span class="">Platzhalter</span>
+				<span class="p-2 rounded-md bg-neutral-800">Positiv</span>
+			</div>
+			<div class="mb-4 flex justify-between">
+				<span class="">Platzhalter</span>
+				<span class="p-2 rounded-md bg-neutral-800">So und so</span>
+			</div>
+		</div>
 
-	<div class="p-4 w-80 text-white">
-		<div class="mb-4 flex justify-between">
-			<div class="">Thema:</div>
-			<div class="p-2 rounded-md bg-neutral-800">Asyl Politik</div>
-		</div>
-		<div class="mb-4 flex justify-between">
-			<span class="">Partei:</span>
-			<span class="p-2 rounded-md bg-blue-700">JUSO</span>
-		</div>
-		<div class="mb-4 flex justify-between">
-			<span class="">Politisches Spektrum:</span>
-			<span class="p-2 rounded-md bg-green-700">65% Links</span>
-		</div>
-		<div class="mb-4 flex justify-between">
-			<span class="">Platzhalter</span>
-			<span class="p-2 rounded-md bg-neutral-800">Positiv</span>
-		</div>
-		<div class="mb-4 flex justify-between">
-			<span class="">Platzhalter</span>
-			<span class="p-2 rounded-md bg-neutral-800">So und so</span>
+		<div class="article-wrapper text-yellow-100 max-w-xl">
+			<div class="article-info-bar flex text-s mb-4">
+				<div class="article-date mr-8">Samstag, 23.12.2023</div>
+				<div class="article-info-title">Stadtparlaments Sitzung</div>
+			</div>
+			<div class="article-header text-xl font-bold mb-4">Untersuchung der Haftbedingungen und Überstellungspraktiken im Ausschaffungsgefängnis Bazenheid</div>
+			<div class="article-abstract italic mb-4">In einer detaillierten Anfrage an das Stadtparlament St. Gallen hinterfragt Andrea Scheck, Stadtparlamentarierin der JUSO, die Haftbedingungen im Ausschaffungsgefängnis Bazenheid und die Praxis der Überstellung abgewiesener Asylsuchender aus St. Gallen in diese Einrichtung.</div>
+			<div class="article-text mb-4">Der Text beinhaltet eine Anfrage an das Stadtparlament St. Gallen bezüglich der Haftbedingungen im Ausschaffungsgefängnis Bazenheid und der Überstellung abgewiesener Asylsuchender dorthin. Die Anfrage thematisiert die Einhaltung von Mindeststandards, mangelnde Alternativen zu diesen Gefängnissen und die Prüfung der Rechtmäßigkeit und Angemessenheit der Haft. Der Stadtrat erkennt an, dass nur Mindestanforderungen erfüllt werden, sieht jedoch keine unmittelbaren Alternativen, bis die geplante Erweiterung des Regionalgefängnisses Altstätten realisiert ist.</div>
 		</div>
 	</div>
 </section>
