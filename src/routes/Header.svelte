da3852fa4b35c9b35f894af495de74b18bfb8dc7
Simon Stratemeier <simon@concept-hero.de>
2023-12-02 17:24:52 +0100

    Change Menu Headers

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -17,10 +17,10 @@
 		</svg>
 		<ul>
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
+				<a href="/">Chart.js</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
+			<li aria-current={$page.url.pathname === '/tailwind' ? 'page' : undefined}>
+				<a href="/tailwind">Tailwind</a>
 			</li>
 			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
 				<a href="/sverdle">Sverdle</a>
