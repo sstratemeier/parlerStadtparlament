a00e8523a7ea3b4232321aceda99aa463442ec55
Till-JS <101404291+Till-JS@users.noreply.github.com>
2023-12-02 22:41:21 +0100

    dashboard

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,36 +1,36 @@
-<nav class="bg-gray-800 p-3 rounded-lg shadow-md mx-auto my-4 max-w-6xl">
+<nav class="bg-gray-800 p-3 rounded-lg shadow-md my-4 w-11/12 self-center">
 	<div class="flex items-center justify-between">
 		<!-- Logo placeholder on the left -->
 		<a
 			href="/"
-			class="text-white font-bold py-2 px-4 rounded hover:bg-gray-700 transition-colors duration-150 no-underline"
+			class="text-white font-bold py-2 px-4 rounded hover:bg-gray-700 transition-colors duration-150 hover:no-underline"
 			role="button"
 		>
-			LOGO
+			Polit Raum
 		</a>
 
 		<!-- Buttons on the right -->
 		<div class="flex space-x-2">
 			<a
-				href="/link1"
-				class="text-white font-semibold py-2 px-4 rounded hover:bg-gray-700 transition-colors duration-150 no-underline"
+				href="/sitzungen"
+				class="text-white font-semibold py-2 px-4 rounded hover:bg-gray-700 transition-colors duration-150 hover:no-underline"
 				role="button"
 			>
-				Button 1
+				Sitzungen
 			</a>
 			<a
-				href="/link2"
-				class="text-white font-semibold py-2 px-4 rounded hover:bg-gray-700 transition-colors duration-150 no-underline"
+				href="/themen"
+				class="text-white font-semibold py-2 px-4 rounded hover:bg-gray-700 transition-colors duration-150 hover:no-underline"
 				role="button"
 			>
-				Button 2
+				Themen
 			</a>
 			<a
-				href="/link3"
-				class="text-white font-semibold py-2 px-4 rounded hover:bg-gray-700 transition-colors duration-150 no-underline"
+				href="/parteien"
+				class="text-white font-semibold py-2 px-4 rounded hover:bg-gray-700 transition-colors duration-150 hover:no-underline"
 				role="button"
 			>
-				Button 3
+				Parteien
 			</a>
 			<!-- Add more buttons as needed -->
 		</div>
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,155 @@
+<div class=" text-white p-4">
+	<!-- Main Grid Container -->
+	<div class="container mx-auto grid grid-cols-1 md:grid-cols-12 gap-4 md:custom-height-md">
+		<!-- Main Content Area -->
+		<div class="md:col-span-8 flex flex-col">
+			<!-- Neueste Sitzung Section -->
+			<div class="bg-zinc-800 p-6 px-8 rounded-3xl mb-4 flex-grow overflow-auto">
+				<!-- Content goes here -->
+				<h2 class="font-bold text-zinc-100 underline text-2xl mb-2">Neueste Sitzung</h2>
+				<p>Samstag, 23.12.2023</p>
+				<h2 class="text-2xl">
+					"Untersuchung der Haftbedingungen und Überstellungspraktiken im Ausschaffungsgefängnis
+					Bazenheid"
+				</h2>
+				<!-- ... Other content -->
+			</div>
+
+			<!-- Themen dieses Jahr Section -->
+			<div class="bg-zinc-800 p-6 px-8 rounded-3xl flex-grow overflow-auto">
+				<!-- Content goes here -->
+				<h2 class="font-bold text-zinc-100 underline text-2xl mb-2">Themen dieses Jahr</h2>
+				<!-- Add your stats bars or other content here -->
+				<!-- ... Other content -->
+			</div>
+		</div>
+
+		<!-- Sidebar Area -->
+		<div class="md:col-span-4 flex flex-col">
+			<!-- Ältere Sitzungen Section -->
+			<div class="bg-zinc-800 p-6 px-8 rounded-3xl flex flex-col h-full mb-4">
+				<!-- Content goes here -->
+				<h2 class="font-bold text-zinc-100 underline text-2xl mb-2 pb-2">Ältere Sitzungen</h2>
+
+				<!-- Buttons container -->
+				<div class="flex flex-col flex-grow space-y-2">
+					<!-- Each main button with a nested right-aligned "Thema" button -->
+					<div
+						class="flex justify-between items-center bg-gray-700 text-white rounded hover:bg-gray-600 text-sm p-2 flex-grow overflow-hidden"
+					>
+						<span class="truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+						<button class="text-white bg-blue-500 hover:bg-blue-400 rounded p-1">Thema</button>
+					</div>
+					<div
+						class="flex justify-between items-center bg-gray-700 text-white rounded hover:bg-gray-600 text-sm p-2 flex-grow overflow-hidden"
+					>
+						<span class="truncate"
+							>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</span
+						>
+						<button class="text-white bg-green-500 hover:bg-green-400 rounded p-1">Thema</button>
+					</div>
+					<div
+						class="flex justify-between items-center bg-gray-700 text-white rounded hover:bg-gray-600 text-sm p-2 flex-grow overflow-hidden"
+					>
+						<span class="truncate">Ut enim ad minim veniam, quis nostrud exercitation ullamco</span>
+						<button class="text-white bg-red-500 hover:bg-red-400 rounded p-1">Thema</button>
+					</div>
+					<div
+						class="flex justify-between items-center bg-gray-700 text-white rounded hover:bg-gray-600 text-sm p-2 flex-grow overflow-hidden"
+					>
+						<span class="truncate">Duis aute irure dolor in reprehenderit in voluptate velit</span>
+						<button class="text-white bg-yellow-500 hover:bg-yellow-400 rounded p-1">Thema</button>
+					</div>
+					<div
+						class="flex justify-between items-center bg-gray-700 text-white rounded hover:bg-gray-600 text-sm p-2 flex-grow overflow-hidden"
+					>
+						<span class="truncate"
+							>Excepteur sint occaecat cupidatat non proident, sunt in culpa</span
+						>
+						<button class="text-white bg-purple-500 hover:bg-purple-400 rounded p-1">Thema</button>
+					</div>
+				</div>
+			</div>
+
+			<div class="bg-zinc-800 p-6 px-8 rounded-3xl shadow-md">
+				<!-- Parteien Section Header -->
+				<div class="flex justify-between items-center mb-4">
+					<h2 class=" font-bold text-zinc-100 underline text-2xl">Parteien</h2>
+					<div class="flex items-center">
+						<span class="truncate text-sm text-white mr-2">Eingaben:</span>
+						<span class="truncate text-sm text-white">Hauptthemen:</span>
+					</div>
+				</div>
+
+				<!-- Parteien List -->
+				<div class="space-y-2">
+					<!-- Party Item -->
+					<div class="flex justify-between items-center">
+						<a
+							href="/gruene"
+							class="text-sm font-bold text-white bg-green-700 rounded py-1 text-center w-20 block text-center"
+							>Grüne</a
+						>
+						<span class="text-sm font-bold text-green-500 mx-2">+11%</span>
+						<button class="text-white bg-gray-600 hover:bg-gray-500 rounded py-1 truncate px-4"
+							>Asyl Politik</button
+						>
+					</div>
+					<!-- Repeat for each party -->
+					<div class="flex justify-between items-center">
+						<a
+							href="/sp"
+							class="text-sm font-bold text-white bg-red-700 rounded py-1 text-center w-20 block text-center"
+							>SP</a
+						>
+						<span class="text-sm font-bold text-green-500 mx-2">+8%</span>
+						<button class="text-white bg-gray-600 hover:bg-gray-500 rounded py-1 truncate px-4"
+							>Asyl Politik</button
+						>
+					</div>
+					<div class="flex justify-between items-center">
+						<a
+							href="/fdp"
+							class="text-sm font-bold text-white bg-blue-700 rounded py-1 text-center w-20 block text-center"
+							>FDP</a
+						>
+						<span class="text-sm font-bold text-green-500 mx-2">+4%</span>
+						<button class="text-white bg-gray-600 hover:bg-gray-500 rounded py-1 truncate px-4"
+							>Asyl Politik</button
+						>
+					</div>
+					<div class="flex justify-between items-center">
+						<a
+							href="/svp"
+							class="text-sm font-bold text-white bg-green-500 rounded py-1 text-center w-20 block text-center"
+							>SVP</a
+						>
+						<span class="text-sm font-bold text-red-500 mx-2">-8%</span>
+						<button class="text-white bg-gray-600 hover:bg-gray-500 rounded py-1 truncate px-4"
+							>Asyl Politik</button
+						>
+					</div>
+					<div class="flex justify-between items-center">
+						<a
+							href="/cvp"
+							class="text-sm font-bold text-white bg-orange-700 rounded py-1 text-center w-20 block text-center"
+							>CVP</a
+						>
+						<span class="text-sm font-bold text-red-500 mx-2">-10%</span>
+						<button class="text-white bg-gray-600 hover:bg-gray-500 rounded py-1 truncate px-4"
+							>Asyl Politik</button
+						>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	@media (min-width: 768px) {
+		.custom-height-md {
+			height: calc(100vh - 100px); /* Full viewport height minus 100px for medium screens and up */
+		}
+	}
+</style>
