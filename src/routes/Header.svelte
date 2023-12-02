3eca18cc6375ff7054cd5115f18286a4e1cff5d9
Till-JS <101404291+Till-JS@users.noreply.github.com>
2023-12-02 18:35:32 +0100

    syn

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,23 +1,38 @@
 <nav class="bg-gray-800 p-3 rounded-lg shadow-md mx-auto my-4 max-w-6xl">
 	<div class="flex items-center justify-between">
-	  <!-- Logo placeholder on the left -->
-	  <a href="/" class="text-white font-bold py-2 px-4 rounded hover:bg-gray-700 transition-colors duration-150 no-underline" role="button">
-		LOGO
-	  </a>
-  
-	  <!-- Buttons on the right -->
-	  <div class="flex space-x-2">
-		<a href="/link1" class="text-white font-semibold py-2 px-4 rounded hover:bg-gray-700 transition-colors duration-150 no-underline" role="button">
-		  Button 1
+		<!-- Logo placeholder on the left -->
+		<a
+			href="/"
+			class="text-white font-bold py-2 px-4 rounded hover:bg-gray-700 transition-colors duration-150 no-underline"
+			role="button"
+		>
+			LOGO
 		</a>
-		<a href="/link2" class="text-white font-semibold py-2 px-4 rounded hover:bg-gray-700 transition-colors duration-150 no-underline" role="button">
-		  Button 2
-		</a>
-		<a href="/link3" class="text-white font-semibold py-2 px-4 rounded hover:bg-gray-700 transition-colors duration-150 no-underline" role="button">
-		  Button 3
-		</a>
-		<!-- Add more buttons as needed -->
-	  </div>
+
+		<!-- Buttons on the right -->
+		<div class="flex space-x-2">
+			<a
+				href="/link1"
+				class="text-white font-semibold py-2 px-4 rounded hover:bg-gray-700 transition-colors duration-150 no-underline"
+				role="button"
+			>
+				Button 1
+			</a>
+			<a
+				href="/link2"
+				class="text-white font-semibold py-2 px-4 rounded hover:bg-gray-700 transition-colors duration-150 no-underline"
+				role="button"
+			>
+				Button 2
+			</a>
+			<a
+				href="/link3"
+				class="text-white font-semibold py-2 px-4 rounded hover:bg-gray-700 transition-colors duration-150 no-underline"
+				role="button"
+			>
+				Button 3
+			</a>
+			<!-- Add more buttons as needed -->
+		</div>
 	</div>
-  </nav>
-  
+</nav>
